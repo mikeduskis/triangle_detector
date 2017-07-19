d3134f69117635a0440d9e46df6aa5d347dbfd84
Mike Duskis <mike.duskis@protectwise.com>
2017-07-19 11:17:40 -0600

    Completed process_request

---
+++ tests/test_process_request.py
@@ -0,0 +1,166 @@
+import json
+from unittest import TestCase, main, skip
+
+from expects import expect, be_a, be, contain, equal
+from werkzeug.wrappers import BaseResponse as Response
+
+
+from triangle_detector.process_request import process_request
+
+
+class FakeRequest:
+    """
+    Fakes Flask request
+    http://flask.pocoo.org/docs/0.12/api/
+    """
+
+    def __init__(self, a=1, b=2, c=3, is_json=True):
+        self.sides = {'a': a, 'b': b, 'c': c}
+        self._is_json = is_json
+
+    def get_json(self, *args, **kawrgs):
+        return self.sides
+
+    def is_json(self):
+        return self._is_json
+
+
+class HandlerSpy:
+
+    def __init__(self):
+        self.a = None
+        self.b = None
+        self.c = None
+
+    def __call__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        return True
+
+
+class TestProcessRequest(TestCase):
+
+    def test_response_is_a_response(self):
+        response = process_request(FakeRequest(), lambda a, b, c: True)
+        expect(response).to(be_a(Response))
+
+    def test_passes_a_from_request_to_handler_as_int(self):
+        expected = 42
+        request = FakeRequest(a=expected)
+        spy = HandlerSpy()
+        process_request(request, spy)
+        expect(spy.a).to(equal(expected))
+
+    def test_passes_b_from_request_to_handler_as_int(self):
+        expected = 43
+        request = FakeRequest(a=42, b=expected, c=99)
+        spy = HandlerSpy()
+        process_request(request, spy)
+        expect(spy.b).to(equal(expected))
+
+    def test_passes_c_from_request_to_handler_as_int(self):
+        expected = 87
+        request = FakeRequest(a=42, b=102, c=expected)
+        spy = HandlerSpy()
+        process_request(request, spy)
+        expect(spy.c).to(equal(expected))
+
+    def test_response_status_is_400_when_request_is_not_json(self):
+        response = process_request(
+            FakeRequest(is_json=False), lambda a, b, c: True)
+        expect(response.status_code).to(equal(400))
+
+    def test_response_status_is_400_when_request_lacks_a(self):
+        request = FakeRequest()
+        request.sides = {'b': 1, 'c': 2}
+        response = process_request(request, lambda a, b, c: True)
+        expect(response.status_code).to(equal(400))
+
+    def test_response_status_is_400_wjen_request_lacks_b(self):
+        request = FakeRequest()
+        request.sides = {'a': 1, 'c': 2}
+        response = process_request(request, lambda a, b, c: True)
+        expect(response.status_code).to(equal(400))
+
+    def test_response_status_is_400_when_request_lacks_c(self):
+        request = FakeRequest()
+        request.sides = {'a': 1, 'b': 2}
+        response = process_request(request, lambda a, b, c: True)
+        expect(response.status_code).to(equal(400))
+
+    def test_response_status_is_400_when_request_a_is_not_a_number(self):
+        request = FakeRequest()
+        request.sides = {'a': 'a', 'b': 2, 'c': 3}
+        response = process_request(request, lambda a, b, c: True)
+        expect(response.status_code).to(equal(400))
+
+    def test_response_status_is_400_when_request_b_is_not_a_number(self):
+        request = FakeRequest()
+        request.sides = {'a': 1, 'b': 'two', 'c': 3}
+        response = process_request(request, lambda a, b, c: True)
+        expect(response.status_code).to(equal(400))
+
+    def test_response_status_is_400_when_request_c_is_not_a_number(self):
+        request = FakeRequest()
+        request.sides = {'a': 1, 'b': 2, 'c': 'the spinach imposition'}
+        response = process_request(request, lambda a, b, c: True)
+        expect(response.status_code).to(equal(400))
+
+    def test_response_has_json_content_type(self):
+        response = process_request(FakeRequest(), lambda a, b, c: True)
+        expect(response.headers['Content-type']).to(equal('application/json'))
+
+    @staticmethod
+    def extract_json(response):
+        try:
+            payload = response.response
+            return json.loads(payload)[0]
+        except Exception as e:
+            raise Exception(
+                'Failed to decode response %s "%s" (%s: %s)'
+                % (
+                    type(response.response),
+                    response.response, type(e), e)) from e
+
+    def test_response_is_json_true_when_handler_returns_true(self):
+        response = process_request(FakeRequest(), lambda a, b, c: True)
+        expect(self.extract_json(response)).to(be(True))
+
+    def test_reponse_status_is_200_when_handler_returns_true(self):
+        response = process_request(FakeRequest(), lambda a, b, c: True)
+        expect(response.status_code).to(equal(200))
+
+    def test_response_is_json_false_when_handler_returns_false(self):
+        response = process_request(FakeRequest(), lambda a, b, c: False)
+        expect(self.extract_json(response)).to(be(False))
+
+    def test_response_status_is_200_when_handler_returns_false(self):
+        response = process_request(FakeRequest(), lambda a, b, c: False)
+        expect(response.status_code).to(equal(200))
+
+    def test_response_has_text_content_type_when_handler_raises_exception(self):
+        def handler(a, b, c):
+            raise Exception('intentional')
+
+        response = process_request(FakeRequest(), handler)
+        expect(response.headers['Content-type']).to(equal('text/plain'))
+
+    def test_response_status_is_500_when_handler_raises_exception(self):
+        def handler(a, b, c):
+            raise Exception('intentional')
+
+        response = process_request(FakeRequest(), handler)
+        expect(response.status_code).to(equal(500))
+
+    def test_response_contains_exception_raised_by_handler(self):
+        expected = "It's a fair cop."
+
+        def handler(a, b, c):
+            raise Exception(expected)
+        response = process_request(FakeRequest(), handler)
+        expect(response.response).to(contain(expected))
+
+
+if '__main__' == __name__:
+    main()
--- triangle_detector/process_request.py
+++ triangle_detector/process_request.py
@@ -1,9 +1,45 @@
+import json
+
 from werkzeug.wrappers import BaseResponse as Response
 
 
-def process_request(request, handler):
+class BadRequest(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+        self.code = 400
+        self.message = msg
 
-    return Response(
-        status=404,
-        response='There is nothing to see here.',
-        content_tyep='text/plain')
+
+def extract_sides(request):
+    if not request.is_json():
+        raise BadRequest('Expected application/json content type')
+    sides = request.get_json()
+    if 'a' not in sides:
+        raise BadRequest('Request lacks "a" side')
+    if 'b' not in sides:
+        raise BadRequest('Request lacks "b" side')
+    if 'c' not in sides:
+        raise BadRequest('Request lacks "c" side')
+    try:
+        return {k: float(sides[k]) for k in ('a', 'b', 'c')}
+    except ValueError as e:
+        raise BadRequest('All sides must be numbers')
+
+
+def process_request(request, handler):
+    response = Response(
+        status=200,
+        content_type='application/json')
+
+    try:
+        sides = extract_sides(request)
+        response.response = json.dumps([handler(**sides)])
+    except BadRequest as e:
+        response.status_code = e.code
+        response.response = e.message
+    except Exception as e:
+        response.status_code = 500
+        response.headers['Content-type'] = 'text/plain'
+        response.response = str(e)
+
+    return response
